cca382e14861279f4b695b3e007768cd6cd176a5
jordanj83 <jordan.johnson@telus.com>
2022-12-13 09:43:40 -0800

    more files

--- BootcampX/2_queries_joins/1_students_total_assignment_duration.sql
+++ BootcampX/2_queries_joins/1_students_total_assignment_duration.sql
@@ -1,4 +1,5 @@
-SELECT sum(assignment_submissions.duration) as total_duration
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
 FROM assignment_submissions
 JOIN students ON students.id = student_id
-WHERE students.name = 'Ibrahim Schimmel';
+WHERE students.end_date IS NULL
+GROUP BY students.name;
